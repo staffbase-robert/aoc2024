1b445554ad6ec4407ec36cc27eb5060f8a5110bd
0x28F4 <robert_hecht@outlook.de>
2024-12-08 16:41:03 +0100

    fix test

--- day08/main_test.go
+++ day08/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"iter"
 	"testing"
 
 	"github.com/staffbase-robert/aoc2024/utils"
@@ -10,10 +11,15 @@ import (
 func TestAntinodes(t *testing.T) {
 	pair := pair{a: c.Point{X: 1, Y: 1}, b: c.Point{X: 2, Y: 2}}
 
-	_, iter := pair.antinodes()
+	_next, stop := iter.Pull(pair.shoot(true))
+	next := func() c.Point {
+		v, _ := _next()
+		return v
+	}
 
-	utils.MustEq(iter(), c.Point{X: 0, Y: 0})
-	utils.MustEq(iter(), c.Point{X: -1, Y: -1})
-	utils.MustEq(iter(), c.Point{X: -2, Y: -2})
-	utils.MustEq(iter(), c.Point{X: -3, Y: -3})
+	utils.MustEq(next(), c.Point{X: 0, Y: 0})
+	utils.MustEq(next(), c.Point{X: -1, Y: -1})
+	utils.MustEq(next(), c.Point{X: -2, Y: -2})
+	utils.MustEq(next(), c.Point{X: -3, Y: -3})
+	stop()
 }
